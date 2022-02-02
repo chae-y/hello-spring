08ab3e42252ae45144d7266c198c3e4867ebf184
chae <stat_chae0@sookmyung.ac.kr>
2022-02-02 17:55:12 +0900

    hello mvc

--- src/main/java/hello/hellospring/controller/helloController.java
+++ src/main/java/hello/hellospring/controller/helloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class helloController {
@@ -10,5 +11,10 @@ public class helloController {
     public String hello(Model model){
         model.addAttribute("data", "hello!");
         return "hello";//templates의 hello.html을 찾아서 랜더링한다.
+    }
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
