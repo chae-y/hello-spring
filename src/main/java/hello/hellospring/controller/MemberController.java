2747d03a25bf52fbcf70c79fa4a1233f1a4ab898
chae <stat_chae0@sookmyung.ac.kr>
2022-02-04 15:41:08 +0900

    등록

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller //컨트롤러에서 외부요청을 받고 서비스에서 비즈니스 로직을 만들고 리파지토리에서 데이터를 저장하고 =>정형화되어있는 패턴 (컴포넌트 스캔 방법)
 public class MemberController {
@@ -13,5 +16,20 @@ public class MemberController {
     @Autowired // 스프링이 멤버서비스를 연결해줌 (의존관계를 주입)
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
